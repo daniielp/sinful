d9c3d14b2884f6154e791423165ea14b5ee815d1
danielP <daniel.horsens@gmail.com>
2023-11-09 16:35:52 +0100

    Added branding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			
+			colors: {
+				sinful: {
+					'light-red': '#FDC7CD',
+					'red': '#E71F33',
+					'green': '#134840',
+					'gray': '#2c2f32',
+					'blue': '#2e45b8'
+				}
+			}
 		},
 	},
 	plugins: [],
